5a8a97a4bdd443c21edd242f8f8aceabec7075af
George 228 <maxkostenko2001@mail.ru>
2022-11-06 19:10:31 +0300

    redit game code to use new way for window refreshing

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import time
 from gui import *
 from cards import *
 from tkinter import Tk
@@ -10,49 +11,50 @@ root = Tk()
 gui = GUI(root)
 gui.clear_table()
 
+
 for i in range(5):
     gui.hide_player_hand(i)
 
-players = [Player(f"Player {i + 1}") for i in range(5)]
-players_queue = PlayersQueue(players)
 
-table = Table()
-deck = Deck()
+def a_main():
+    players = [Player(f"Player {i + 1}") for i in range(5)]
+    players_queue = PlayersQueue(players)
 
-deck.shuffle()
-table.draw(deck, 3)
-gui.set_table_cards(*table.hand, places=(0, 1, 2))
+    table = Table()
+    deck = Deck()
 
-for i, p in enumerate(players_queue.list):
-    p.draw(deck, 2)
-    gui.set_player_hand(i, p.hand)
-    p.hand_rank(table)
+    deck.shuffle()
+    table.draw(deck, 3)
+    gui.set_table_cards(*table.hand, places=(0, 1, 2))
 
-gui.show_players_hand([2])
-gui.set_rank(players_queue.list[2].rank.name)
-
-
-ranks = []
-for i in range(3, 5):
-    table.draw(deck, 1)
-    for p in players_queue.list:
+    for i, p in enumerate(players_queue.list):
+        p.draw(deck, 2)
+        gui.set_player_hand(i, p.hand)
         p.hand_rank(table)
-    ranks.append(players_queue.list[2].rank.name)
 
-winners = rank_comparison(table, *players)
+    gui.show_players_hand([2])
+    gui.set_rank(players[2].rank.name)
 
-gui.delay(2, lambda: gui.set_table_card(table.hand[-2], 3))
-gui.delay(4, lambda: gui.set_table_card(table.hand[-1], 4))
+    time.sleep(5)
+    for i in range(3, 5):
+        table.draw(deck, 1)
+        gui.set_table_card(table.hand[-1], i)
+        gui.set_rank(players[2].rank.name)
+        time.sleep(5)
+        for p in players_queue.list:
+            p.hand_rank(table)
 
-gui.delay(2, lambda: gui.set_rank(ranks[0]))
-gui.delay(4, lambda: gui.set_rank(ranks[1]))
+    winners = rank_comparison(table, *players)
 
-gui.delay(6, lambda: gui.show_message(f'WIN - {winners} with {winners[0].rank.name}', 'black'))
+    gui.show_message(f'WIN - {winners} with {winners[0].rank.name}', 'black')
 
-winners_indexes = []
-for i in winners:
-    winners_indexes.append(players.index(i))
+    winners_indexes = []
+    for i in winners:
+        winners_indexes.append(players.index(i))
 
-gui.delay(6, lambda: gui.show_players_hand(winners_indexes))
+    gui.show_players_hand(winners_indexes)
 
+
+gui.start(a_main)
 gui.start_mainloop()
+
